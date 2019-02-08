83ec6d2ba3103f7500dabc120f4cc361e6d489f7
shreegowtham27 <gforgowtham8@gmail.com>
2019-02-08 18:41:37 +0530

    Have Added Hamburger to the Navigation Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Menu = () => (
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+    <div className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="navbar collapse navbar-collapse navbar-expand-lg effect-main" id="navbarSupportedContent">
+
         <ul className="navbar-nav ml-auto navbar-right">
             <li className="nav-item">
                 <Link to="/#how-it-works" className="nav-link js-scroll-trigger">How it Works</Link>
@@ -18,6 +25,8 @@ const Menu = () => (
             </li>
         </ul>
     </div>
+</div>
+    
 )
 
 export default Menu;
---
+++ src/pages/testpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+//import { Link } from 'gatsby'
+import Menu from '../components/menu'
+
+
+const TestPage = () => (
+<Layout>
+    <Menu />
+</Layout>
+)
+
+export default TestPage;
